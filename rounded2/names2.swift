ef96af3f6727b1a775d0e5a7f4738ddbe101ae5d
Amal44b <amal4488vvvv@gmail.com>
2023-12-28 11:38:35 +0300

    ...

--- rounded2/names2.swift
+++ rounded2/names2.swift
@@ -11,59 +11,68 @@ struct names2: View {
     @State private  var name:String=""
     @State private  var age:String=""
     var body: some View {
-        VStack {
-            HStack {
-                                Spacer()
-                                Text("اسم الطفل")
-                                    .font(.title)
-                            }
-                            .multilineTextAlignment(.trailing)
-                        TextField("اكتب اسم الطفل", text: $name )
-                                        .textFieldStyle(.roundedBorder)
-                                        //.padding()
-                                        .multilineTextAlignment(.trailing)
-                                        .overlay(
-                                            
-                                    RoundedRectangle(cornerRadius: 8)
-                            .stroke(Color.black, lineWidth: 1)// تعيين لون الحدود بالأسود
-                                                        )
-            
-            
-            HStack {
-                                Spacer()
-                                Text("عمر الطفل")
-                                    .font(.title)
-                            }
-                            .multilineTextAlignment(.trailing)
-                        TextField("اكتب عمر الطفل", text: $age )
-                                        .textFieldStyle(.roundedBorder)
-                                        //.padding()
-                                        .multilineTextAlignment(.trailing)
-                                        .overlay(
-                                            
-                                    RoundedRectangle(cornerRadius: 8)
-                            .stroke(Color.black, lineWidth: 1)// تعيين لون الحدود بالأسود
-                                                        )
         
-            HStack{
-                Rectangle()
-                    .foregroundColor(.clear)
-                    .frame(width: 42, height: 25)
-                    .background(Color(red: 0.67, green: 0.8, blue: 0.78))
-                    .cornerRadius(5)
-                    .shadow(color: .black.opacity(0.25), radius: 2, x: 0, y: 4)
+        NavigationView{
+            VStack {
+                HStack {
+                    Spacer()
+                    Text("اسم الطفل")
+                        .font(.title)
+                }
+                .multilineTextAlignment(.trailing)
+                TextField("اكتب اسم الطفل", text: $name )
+                    .textFieldStyle(.roundedBorder)
+                //.padding()
+                    .multilineTextAlignment(.trailing)
+                    .overlay(
+                        
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.black, lineWidth: 1)// تعيين لون الحدود بالأسود
+                    )
                 
                 
-                Rectangle()
-    
-                  .foregroundColor(.clear)
-                  .frame(width: 42, height: 25)
-                  .background(Color(red: 0.67, green: 0.8, blue: 0.78))
-                  .cornerRadius(5)
-                  .shadow(color: .black.opacity(0.25), radius: 2, x: 0, y: 4)
+                HStack {
+                    Spacer()
+                    Text("عمر الطفل")
+                        .font(.title)
+                }
+                .multilineTextAlignment(.trailing)
+                TextField("اكتب عمر الطفل", text: $age )
+                    .textFieldStyle(.roundedBorder)
+                //.padding()
+                    .multilineTextAlignment(.trailing)
+                    .overlay(
+                        
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.black, lineWidth: 1)// تعيين لون الحدود بالأسود
+                    )
                 
-            }
-            
+                HStack{
+                    
+                    Button(action: {
+                        // أضف الإجراء الذي ترغب في تنفيذه عند النقر على الزر هنا
+                    }) {
+                        Text("إلغاء")
+                            .foregroundColor(.white)
+                            .padding(.horizontal, 10)
+                            .padding(.vertical, 5)
+                            .background(Color(red: 0.67, green: 0.8, blue: 0.78))
+                            .cornerRadius(5)
+                    }
+                    Button(action: {
+                        // أضف الإجراء الذي ترغب في تنفيذه عند النقر على الزر هنا
+                    }) {
+                        Text("إضافة")
+                            .foregroundColor(.white)
+                            .padding(.horizontal, 10)
+                            .padding(.vertical, 5)
+                            .background(Color(red: 0.67, green: 0.8, blue: 0.78))
+                            .cornerRadius(5)
+                    }
+                    
+                } .padding(.top, 20.0)
+                
+            }  .padding()
         }
     }
 }
