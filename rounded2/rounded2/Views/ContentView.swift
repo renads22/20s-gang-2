fa3432cd9e5b4a71fff2b380d8e67211af79dd6e
Amal44b <amal4488vvvv@gmail.com>
2024-10-25 04:26:53 +0300

    fix + buttom issus on ipad

--- rounded2/rounded2/Views/ContentView.swift
+++ rounded2/rounded2/Views/ContentView.swift
@@ -9,80 +9,86 @@ import SwiftData
 import UserNotifications
 
 struct ContentView: View {
-    @Environment(\.modelContext) var modelContext
-    @Query var reminders: [Riminder]
-    
-   init(){
+   @Environment(\.modelContext) var modelContext
+   @Query var reminders: [Riminder]
+   
+   init() {
       UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.black]
    }
    
-    var body: some View {
-        NavigationView {
-            ZStack {  // Use ZStack to layer the background image and content
-                Image("background1")  // Your background image
-                    .resizable()
-                    .scaledToFill()  // Ensures the image fills the entire screen
-                    .ignoresSafeArea()  // Extend the image to cover safe area insets
-                
-                VStack(spacing: 20) {
-                    NavigationLink(destination: BabyInfo()) {
-                        Image(systemName: "plus.circle")
-                            .font(.system(size: 45))
-                            .shadow(color: .black.opacity(0.25), radius: 2, x: 0, y: 4)
-                            .padding(.leading, 300)
-                            //.navigationTitle("BabyTasks")
-                            .navigationBarTitle("BabyTasks", displayMode: .inline)
-                            .foregroundColor(Color("TextColor"))
-                        
-                    }
-                    
-                    ScrollView {
+   var body: some View {
+      NavigationView {
+         ZStack {
+            Image("background1")
+               .resizable()
+               .scaledToFill()
+               .ignoresSafeArea()
+            
+            VStack(spacing: 20) {
+               NavigationLink(destination: BabyInfo()) {
+                  Image(systemName: "plus.circle")
+                     .font(.system(size: 55))
+                     .shadow(color: .black.opacity(0.25), radius: 2, x: 0, y: 4)
+                     .padding(.leading, 300)
+                     .navigationBarTitle("BabyTasks", displayMode: .inline)
+                     .foregroundColor(Color("TextColor"))
+               }
+               
+               // Use GeometryReader to adapt layout
+               GeometryReader { geometry in
+                  ScrollView {
+                     VStack(spacing: 10) { // Adjust spacing if needed
                         ForEach(reminders) { item in
-                            RoundedRectangle(cornerRadius: 18)
-                                .frame(width: 300, height: 100)
-                                .foregroundColor(Color.color2)
-                                .shadow(color: .black.opacity(0.25), radius: 2, x: 0, y: 4)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 18)
-                                        .stroke(Color.black, lineWidth: 2)
-                                )
-                                .overlay(
-                                    HStack {
-                                        VStack {
-                                            Text(item.title)
-                                                .foregroundColor(.white)
-                                                .font(.title)
-                                            Text("")
-                                                .foregroundColor(.black)
-                                                .font(.subheadline)
-                                                .offset(y: 5)
-                                        }
-                                        Spacer()
-                                        Button(role: .destructive) {
-                                            withAnimation {
-                                                if let requestIdentifier = item.requestIdentifier {
-                                                    UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [requestIdentifier])
-                                                }
-                                                modelContext.delete(item)
-                                            }
-                                        } label: {
-                                            Label("Delete", systemImage: "trash")
-                                                .symbolVariant(.fill)
-                                        }
+                           RoundedRectangle(cornerRadius: 18)
+                              .frame(width: geometry.size.width - 40, height: 100) // Adjust width based on geometry
+                              .foregroundColor(Color.color2)
+                              .shadow(color: .black.opacity(0.25), radius: 2, x: 0, y: 4)
+                              .overlay(
+                                 RoundedRectangle(cornerRadius: 18)
+                                    .stroke(Color.black, lineWidth: 2)
+                              )
+                              .overlay(
+                                 HStack {
+                                    VStack {
+                                       Text(item.title)
+                                          .foregroundColor(.white)
+                                          .font(.title)
+                                       Text("")
+                                          .foregroundColor(.black)
+                                          .font(.subheadline)
+                                          .offset(y: 5)
                                     }
+                                    Spacer()
+                                    Button(role: .destructive) {
+                                       withAnimation {
+                                          if let requestIdentifier = item.requestIdentifier {
+                                             UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [requestIdentifier])
+                                          }
+                                          modelContext.delete(item)
+                                       }
+                                    } label: {
+                                       Label("Delete", systemImage: "trash")
+                                          .symbolVariant(.fill)
+                                    }
+                                 }
                                     .padding(.horizontal)
-                                )
-                                .padding()
+                              )
+                              .padding()
                         }
-                    }
-                }
-                .padding()
+                     }
+                     .padding(.bottom) // Add some padding at the bottom
+                  }
+                  .frame(width: geometry.size.width, height: geometry.size.height - 50) // Adjust height accordingly
+               }
+               .frame(maxWidth: .infinity, maxHeight: 600) // Make sure it takes available height
             }
-        }
-        .navigationBarBackButtonHidden(true)
-    }
+            .padding()
+         }
+      }
+      .navigationBarBackButtonHidden(true)
+   }
 }
 
 #Preview {
-    ContentView()
+   ContentView()
 }
